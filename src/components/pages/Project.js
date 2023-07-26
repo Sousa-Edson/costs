56fb811196459928ff07be69b0c8c7b49db0e49e
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-07-26 00:21:29 -0300

    Removendo serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -96,9 +96,33 @@ function Project() {
                 setType('success')
             })
     }
-    function removeService() {
 
+    function removeService(id, cost) {
+        const servicesUpdated = project.services.filter(
+            (service) => service.id !== id,
+        )
+
+        const projectUpdated = project
+
+        projectUpdated.services = servicesUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(projectUpdated),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(projectUpdated)
+                setServices(servicesUpdated)
+                setMessage('Serviço removido com sucesso!')
+            })
     }
+
+
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
